6d39c49d665ca77f7db491043de78f9710297589
Glitch (web-dev-template-glitch-tut) <none>
2023-06-27 21:16:25 +0000

    addplaylist form, methods added

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,9 +1,18 @@
+import { playlistStore } from "../models/playlist-store.js";
+
 export const dashboardController = {
   async index(request, response) {
     const viewData = {
-      title: "Template Application",
+      title: 'Playlist 1 Dashboard',
     };
     console.log("dashboard rendering");
     response.render("dashboard-view", viewData);
+  },
+  async addPlaylist(request, response) {
+    const newPlaylist = {
+      title: request.body.title,
+    };
+    console.log(`adding playlist ${newPlaylist.title}`);
+    response.redirect("/dashboard");
   },
 };
