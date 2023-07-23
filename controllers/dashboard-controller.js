3873d58872a674d51683d03c488f02c4a7fda6f1
Glitch (web-dev-template-glitch-tut) <none>
2023-07-23 15:41:08 +0000

    😵🛤️ Updated with Glitch - Sessions tut complete

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,18 +1,22 @@
 import { playlistStore } from "../models/playlist-store.js";
+import { accountsController } from "./accounts-controller.js";
 
 export const dashboardController = {
   async index(request, response) {
+    const loggedInUser = await accountsController.getLoggedInUser(request);
     const viewData = {
       title: 'Playlist Dashboard',
-      playlists: await playlistStore.getAllPlaylists(),
+      playlists: await playlistStore.getPlaylistsByUserId(loggedInUser._id),
     };
     console.log("dashboard rendering");
     response.render("dashboard-view", viewData);
   },
   
   async addPlaylist(request, response) {
+    const loggedInUser = await accountsController.getLoggedInUser(request);
     const newPlaylist = {
       title: request.body.title,
+      userid: loggedInUser._id,
     };
     console.log(`adding playlist ${newPlaylist.title}`);
     await playlistStore.addPlaylist(newPlaylist);
--- routes.js
+++ routes.js
@@ -1,16 +1,23 @@
 import express from "express";
+import { accountsController } from "./controllers/accounts-controller.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
-import { aboutController } from "./controllers/about-controller.js";
 import { playlistController } from "./controllers/playlist-controller.js";
+import { aboutController } from "./controllers/about-controller.js";
 
 export const router = express.Router();
 
-router.get("/", dashboardController.index);
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
+
 router.get("/dashboard", dashboardController.index);
-router.get("/about", aboutController.index);
 router.post("/dashboard/addplaylist", dashboardController.addPlaylist);
+router.get("/dashboard/deleteplaylist/:id", dashboardController.deletePlaylist);
 router.get("/playlist/:id", playlistController.index);
 router.post("/playlist/:id/addtrack", playlistController.addTrack);
-router.get("/dashboard/deleteplaylist/:id", dashboardController.deletePlaylist);
 router.get("/playlist/:playlistid/deletetrack/:trackid", playlistController.deleteTrack);
+router.get("/about", aboutController.index);
 
